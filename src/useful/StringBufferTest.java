91165a75b35e7cb930923f4ba59d8f7265a17166
LOSTANA <jyj970602@naver.com>
2024-04-26 12:02:16 +0900

    4월 26일 2차

--- src/useful/StringBufferTest.java
+++ src/useful/StringBufferTest.java
@@ -2,27 +2,27 @@ package useful;
 
 public class StringBufferTest {
 
-	//코드의 시작점
+	// 코드의 시작점
 	public static void main(String[] args) {
 		String str1 = new String("Hello");
 		String str2 = new String("World");
-		
+
 		StringBuffer bufferStr = new StringBuffer("Hello");
 		System.out.println(bufferStr);
 		// 1.
 		System.out.println(System.identityHashCode(bufferStr));
-		
+
 		bufferStr.append(str2);
-		
+
 		System.out.println(bufferStr);
-		//2.
+		// 2.
 		System.out.println(System.identityHashCode(bufferStr));
 
-		//1번 결과와 2번 결과에 주소값이 같다라는 의미는
-		//새로운 메모리를 할당 한 것이 아닌 변경한 것이다
-		
-		//활용
-		String newStr = bufferStr.toString();
-	}//end of main
+		// 1번 결과와 2번 결과에 주소값이 같다라는 의미는
+		// 새로운 메모리를 할당 한 것이 아닌 변경한 것이다
 
-}//end of class
+		// 활용
+		String newStr = bufferStr.toString();
+	}// end of main
+
+}// end of class
